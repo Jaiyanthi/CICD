5f6234c022b61eec7a5f8ad33fec937c439cc5a0
IGD service account 36 <TestSolutionCommitterService@microsoft.com>
2019-08-21 10:46:11 +0000

    Release21/08/2019 10:37:44

--- MultilpleSolutionsImport.ps1
+++ MultilpleSolutionsImport.ps1
@@ -28,6 +28,7 @@ Import-Module $env:DOWNLOADSECUREFILE3_SECUREFILEPATH
 Import-Module $env:DOWNLOADSECUREFILE4_SECUREFILEPATH
 Import-Module $env:DOWNLOADSECUREFILE5_SECUREFILEPATH
 Import-Module $env:DOWNLOADSECUREFILE6_SECUREFILEPATH
+Import-Module $env:DOWNLOADSECUREFILE7_SECUREFILEPATH
 }
 
 Write-Host $solutionImportPath
@@ -62,7 +63,7 @@ $solutionInfo = Get-XrmSolutionInfoFromZip -SolutionFilePath $file.FullName
 
         $importJobId = [guid]::NewGuid()
     
-        $asyncOperationId = Import-XrmSolution -ConnectionString "$crmConnectionString" -SolutionFilePath $file.FullName -publishWorkflows $publishWorkflows -overwriteUnmanagedCustomizations $overwriteUnmanagedCustomizations -SkipProductUpdateDependencies $skipProductUpdateDependencies -ConvertToManaged $convertToManaged -HoldingSolution $holdingSolution -ImportAsync $true -WaitForCompletion $true -ImportJobId $importJobId -AsyncWaitTimeout $AsyncWaitTimeout -Verbose
+        $asyncOperationId = Import-XrmSolution -ConnectionString "$crmConnectionString" -SolutionFilePath $file.FullName -publishWorkflows $publishWorkflows -overwriteUnmanagedCustomizations $overwriteUnmanagedCustomizations -SkipProductUpdateDependencies $skipProductUpdateDependencies -ConvertToManaged $convertToManaged -HoldingSolution $holdingSolution -ImportAsync $true -ImportJobId $importJobId -AsyncWaitTimeout $AsyncWaitTimeout -Verbose
    
         Write-Host "Solution Import Completed. Import Job Id: $importJobId"
 
