8692a5b01156177ea00d4c36582e0ffe244902de
Jaiyanthi <50569300+Jaiyanthi@users.noreply.github.com>
2019-09-12 11:03:16 +0530

    Update SolutionToBeImported.ps1

--- DD365/SolutionToBeImported.ps1
+++ DD365/SolutionToBeImported.ps1
@@ -1,13 +1,23 @@
-﻿#
+#
 # Filename: SolutionToBeImported.ps1.
 #
 param(
-$solutionListFile
+$solutionListFile,
+$configListFile
 )
         foreach($solution in [System.IO.File]::ReadLines($solutionListFile)){
 $solutionFileName +=  "$solution" + ";"
         }
 
+		foreach($sourceControlId in [System.IO.File]::ReadLines($configListFile)){
+$dynamicsSourceControlId +=  "$sourceControlId" + ";"
+        }
+
 $newlineDelimited = $solutionFileName -replace ';', "%0D%0A"
+$newlineDelimitedConfigValue = $dynamicsSourceControlId -replace ';', "%0D%0A"
+
+Write-Output $newlineDelimited
+Write-Output $newlineDelimitedConfigValue
 
 Write-Host "##vso[task.setvariable variable=SolutionsFileName]$newlineDelimited"
+Write-Host "##vso[task.setvariable variable=ConfigIds]$newlineDelimitedConfigValue"
