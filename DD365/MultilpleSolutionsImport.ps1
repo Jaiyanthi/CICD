99e8e6cee6338dd382b6d248c36888b56f4a9649
Jaiyanthi <50569300+Jaiyanthi@users.noreply.github.com>
2019-09-25 11:21:24 +0530

    Update MultilpleSolutionsImport.ps1

--- DD365/MultilpleSolutionsImport.ps1
+++ DD365/MultilpleSolutionsImport.ps1
@@ -23,7 +23,7 @@ Write-Verbose 'Entering ImportSolution.ps1'
 Write-Output "Start Multiple solution import..."
 Write-Output $solutionImportPath
 Write-Output $dllPath
-#$dllPath="D:\a\r1\a\_CMR Solution-CI1\drop\AssemblyStorage"
+$dllPath="D:\a\r1\a/_CMR Solution-CI1/drop/AssemblyStorage"
 if(-Not (Get-Module -ListAvailable -Name Xrm.Framework.CI.PowerShell.Cmdlets))
 {
    $dllNames = get-childitem -path $dllPath |select-object FullName
